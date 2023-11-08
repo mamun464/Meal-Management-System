b536391d2ea9f594164be0e5cdd693f3cb77f807
Mamun <mamunurrashid.s.bd@gmail.com>
2023-11-08 11:18:19 +0600

    New App Create

--- MealAPP/urls.py
+++ MealAPP/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/',include('account.urls')),
+    path('api/hostel/',include('HostelApp.urls')),
     # path('api/user/',include('account.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 
 # from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 from account.views import UserRegistrationView,UserLoginView,UserProfileView,UserPasswordChangeView,SendPasswordResetEmailView,UserPasswordResetView,UserDeleteView,UserEditView
 
